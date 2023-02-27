9c1e3697da83bf22f3393047f573eed1ea331092
zeshannaveed568 <zeshannaveed568@gmail.com>
2023-02-28 03:30:09 +0500

    Input Send Message

--- src/components/InputBox/Index.js
+++ src/components/InputBox/Index.js
@@ -2,15 +2,35 @@ import { View, Text } from "react-native";
 import { StyleSheet } from "react-native";
 import { AntDesign, MaterialIcons } from "@expo/vector-icons";
 import { TextInput } from "react-native";
+import { useState } from "react";
 
 const InputBox = () => {
+  const [newMessage, setNewMessage] = useState("");
+
+  const onSend = () => {
+    console.warn("Sending: " + newMessage);
+
+    setNewMessage("");
+  };
+
   return (
     <View style={styles.container}>
       <AntDesign name='plus' size={20} color='royalblue' />
 
-      <TextInput style={styles.input} placeholder='Type a message...' />
+      <TextInput
+        onChangeText={setNewMessage}
+        value={newMessage}
+        style={styles.input}
+        placeholder='Type a message...'
+      />
 
-      <MaterialIcons style={styles.send} name='send' size={16} color='white' />
+      <MaterialIcons
+        onPress={onSend}
+        style={styles.send}
+        name='send'
+        size={16}
+        color='white'
+      />
     </View>
   );
 };
